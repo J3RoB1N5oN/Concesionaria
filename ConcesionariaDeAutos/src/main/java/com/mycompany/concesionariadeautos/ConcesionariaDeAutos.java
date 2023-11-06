8ec73ff23a0bd49244c013a714b21d7a6896867e
J3RoB1N5oN <113572667+J3RoB1N5oN@users.noreply.github.com>
2023-11-06 19:24:23 -0300

    Add files via upload
    clase automovil esta de mas

---
+++ ConcesionariaDeAutos/src/main/java/com/mycompany/concesionariadeautos/ConcesionariaDeAutos.java
@@ -0,0 +1,12 @@
+package com.mycompany.concesionariadeautos;
+
+import com.mycompany.concesionariadeautos.gui.Pantalla;
+
+public class ConcesionariaDeAutos {
+
+    public static void main(String[] args) {
+        Pantalla principal = new Pantalla();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+    }
+}
